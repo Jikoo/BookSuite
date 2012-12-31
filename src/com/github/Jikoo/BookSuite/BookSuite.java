8a03e3fccf5666b0029f5ae9ba37279265bacf78
Jikoo <jikoo.games@gmail.com>
2012-12-31 01:19:21 -0500

    Reorganized, added more permissions and features
    Added perms:
    - booksuite.* - default op
    - booksuite.copy.self - default true
    - booksuite.copy.other - default op
    - booksuite.denycopy - default false
    Be careful assigning booksuite.denycopy, it will override
    booksuite.copy!

--- src/com/github/Jikoo/BookSuite/BookSuite.java
+++ src/com/github/Jikoo/BookSuite/BookSuite.java
@@ -1,40 +1,50 @@
 package com.github.Jikoo.BookSuite;
 
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class BookSuite extends JavaPlugin implements Listener{
+	String version = "1.1.0";
 	@Override
     public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
-		getLogger().info("BookSuite v1.0.0 enabled!");
+		getLogger().info("BookSuite v"+version+" enabled!");
     }
 	@Override
     public void onDisable() {
-		getLogger().info("BookSuite v1.0.0 disabled!");
+		getLogger().info("BookSuite v"+version+" disabled!");
     }
 	@SuppressWarnings("deprecation")
-	@EventHandler
+	@EventHandler //copy book (PrintingPress)
 	public void onPlayerInteract(PlayerInteractEvent event){
-		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
-			if (event.getClickedBlock().getType().equals(Material.WORKBENCH) && event.getPlayer().getItemInHand().getType().equals(Material.WRITTEN_BOOK)){
-				if (!event.getPlayer().hasPermission("booksuite.copy"))
-					event.getPlayer().sendMessage(ChatColor.DARK_RED+"You do not have permission to copy books!");
-				else if (event.getPlayer().getInventory().firstEmpty() == -1)
-					event.getPlayer().sendMessage(ChatColor.DARK_RED+"Inventory full!");
-				else {
-					event.getPlayer().sendMessage(ChatColor.DARK_GREEN+"Book copied!");
-					event.getPlayer().getInventory().addItem(event.getPlayer().getItemInHand());
-					event.getPlayer().updateInventory();
-				}
-			event.setCancelled(true);
+		if (event.getPlayer().hasPermission("booksuite.denycopy"))
+			return;
+		if (event.getPlayer().getItemInHand().getType().equals(Material.WRITTEN_BOOK) && event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.WORKBENCH)){
+			Player p = event.getPlayer();
+			ItemStack i = p.getItemInHand();
+			BookMeta b = (BookMeta) i.getItemMeta();
+			if (!p.hasPermission("booksuite.copy.self"))
+				p.sendMessage(ChatColor.DARK_RED+"You do not have permission to copy books!");
+			else if (!p.hasPermission("booksuite.copy.other") && !b.getAuthor().equals(p.getName()))
+				p.sendMessage(ChatColor.DARK_RED+"You do not have permission to copy others' books!");
+			else if (p.getInventory().firstEmpty() == -1)
+				p.sendMessage(ChatColor.DARK_RED+"Inventory full!");
+			else {
+				p.sendMessage(ChatColor.DARK_GREEN+"Book copied!");
+				p.getInventory().addItem(i);
+				p.updateInventory();
 			}
+			event.setCancelled(true);
 		}
 	}
 }
