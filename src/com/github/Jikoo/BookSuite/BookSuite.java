74157ccf0392d2d29a2eafbbbb920a8b2aabcb3a
Jikoo <jikoo.games@gmail.com>
2013-05-10 00:51:00 -0400

    Changelog updated
    I'd like to actually manage a release in the near future despite my
    cramped schedule, removed some goals.
    Probably won't consider including private saves until books are saved to
    a database, at which point my lazy method of encryption would work quite
    well.

--- src/com/github/Jikoo/BookSuite/BookSuite.java
+++ src/com/github/Jikoo/BookSuite/BookSuite.java
@@ -3,7 +3,6 @@ package com.github.Jikoo.BookSuite;
 import java.io.File;
 import java.io.IOException;
 
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
