d7230bdc367ccd462c7d4ff9fa7da2a69f54893f
Ted Meyer <tmathmeyer@gmail.com>
2013-09-05 14:01:40 -0400

    mailtime 2

--- src/com/github/Jikoo/BookSuite/mail/MailBox.java
+++ src/com/github/Jikoo/BookSuite/mail/MailBox.java
@@ -10,29 +10,63 @@
  ******************************************************************************/
 package com.github.Jikoo.BookSuite.mail;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class MailBox {
+	private static Map<String, MailBox> boxes = new HashMap<String, MailBox>();
+
+	public static MailBox getMailBox(Player p) {
+		return MailBox.getMailBox(p.getName());
+	}
+
+	public static MailBox getMailBox(String p) {
+		if (boxes.get(p) == null) {
+			boxes.put(p, new MailBox(p, 5));
+		}
+		return boxes.get(p);
+	}
+
 	private String user;
 	private int maxSize;
 	private List<BookMailWrapper> inventory = new LinkedList<BookMailWrapper>();
-	
-	public MailBox(String user)
-	{
+
+	public MailBox(String user, int size) {
 		this.user = user;
+		this.maxSize = size;
 	}
-	
-	public void sendMail()
-	{
-		
+
+	public void sendMail() {
+
 	}
-	
-	public void getMail()
-	{
-		if (this.inventory.size() < this.maxSize)
-		{
-			PostalService.getInstance().distribute(user, this.maxSize-this.inventory.size());
+
+	public void getMail() {
+		if (this.inventory.size() < this.maxSize) {
+			PostalService.getInstance().distribute(user,
+					this.maxSize - this.inventory.size());
 		}
 	}
+
+	public Inventory open(Player p) {
+		Inventory mailbox = Bukkit.createInventory(p, maxSize, p.getName()
+				+ "'s MailBox");
+		getMail();
+
+		for (BookMailWrapper bwm : this.inventory) {
+			ItemStack is = new ItemStack(Material.WRITTEN_BOOK);
+			is.setItemMeta(bwm.getAllMeta());
+			mailbox.addItem(is);
+		}
+
+		return mailbox;
+	}
+
 }
--- src/com/github/Jikoo/BookSuite/mail/PostalService.java
+++ src/com/github/Jikoo/BookSuite/mail/PostalService.java
@@ -19,50 +19,41 @@ import org.bukkit.entity.Player;
 
 public class PostalService {
 	Map<String, List<BookMailWrapper>> inventory = new HashMap<String, List<BookMailWrapper>>();
-	
+
 	private static PostalService instance;
-	
-	public static PostalService getInstance()
-	{
-		if (instance == null)
-		{
+
+	public static PostalService getInstance() {
+		if (instance == null) {
 			instance = new PostalService();
 		}
 		return instance;
 	}
-	
-	
-	public void collect(List<BookMailWrapper> mail)
-	{
-		for(BookMailWrapper bmw : mail)
-		{
+
+	public void collect(List<BookMailWrapper> mail) {
+		for (BookMailWrapper bmw : mail) {
 			this.collect(bmw);
 		}
 	}
-	
-	public void collect(BookMailWrapper bmw)
-	{
-		if (this.inventory.get(bmw.getAdressee())==null)
-		{
-			this.inventory.put(bmw.getAdressee(), new LinkedList<BookMailWrapper>());
+
+	public void collect(BookMailWrapper bmw) {
+		if (this.inventory.get(bmw.getAdressee()) == null) {
+			this.inventory.put(bmw.getAdressee(),
+					new LinkedList<BookMailWrapper>());
 		}
 		this.inventory.get(bmw.getAdressee()).add(bmw);
 	}
-	
-	public List<BookMailWrapper> distribute(Player p, int amount)
-	{
+
+	public List<BookMailWrapper> distribute(Player p, int amount) {
 		return this.distribute(p.getName(), amount);
 	}
-	
-	public List<BookMailWrapper> distribute(String p, int amount)
-	{
+
+	public List<BookMailWrapper> distribute(String p, int amount) {
 		List<BookMailWrapper> mail = new LinkedList<BookMailWrapper>();
 		List<BookMailWrapper> allMail = this.inventory.get(p);
-		while(mail.size()<=amount && allMail!=null && allMail.size() > 0)
-		{
+		while (mail.size() <= amount && allMail != null && allMail.size() > 0) {
 			mail.add(allMail.remove(0));
 		}
-		
+
 		return mail;
 	}
 }
