4844e39e2ed887abe4a16c8f2f51c637d314ea06
Ted Meyer <tmathmeyer@gmail.com>
2013-09-16 00:29:45 -0400

    added license to JSON

--- src/com/github/Jikoo/BookSuite/struct/json/JsonBoolean.java
+++ src/com/github/Jikoo/BookSuite/struct/json/JsonBoolean.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Ted Meyer
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package com.github.Jikoo.BookSuite.struct.json;
 
 public class JsonBoolean extends JsonValue{
